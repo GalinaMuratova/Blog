736eba0439369acf86c7214bfdbaf9eaea10e13b
GalinaMuratova <ufkz200147@gmail.com>
2023-06-29 02:11:46 +0600

    Подготовила проект. Удалила лишние файлы. Сделала первоначальную стуктуру. Прописла имитацию перехода по страницам.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./container/Home/Home";
+import Add from "./container/Add/Add";
+import About from "./container/About/About";
+import Contacts from "./container/Contacts/Contacts";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+       <header><NavBar /></header>
+       <Routes>
+           <Route path='/' element={(
+               <Home />
+           )}/>
+           <Route path='/new-post' element={(
+               <Add />
+           )} />
+           <Route path='/about' element={(
+               <About />
+           )} />
+           <Route path='/contacts' element={(
+               <Contacts />
+           )} />
+       </Routes>
+       <footer></footer>
+   </>
   );
 }
 
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <div className='header d-flex justify-content-around'>
+            <h2>My blog</h2>
+            <nav className="main-nav">
+                <ul className='nav-list'>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/new-post">Add</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contacts">Contacts</NavLink></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
